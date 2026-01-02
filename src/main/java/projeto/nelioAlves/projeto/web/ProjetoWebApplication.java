b55c986a02379611add06b466e8300dde0da7f6c
LukinhasLk <lucasrodrigues.10@icloud.com>
2026-01-02 07:10:14 -0300

    começo do projeto SpringBoot

---
+++ src/main/java/projeto/nelioAlves/projeto/web/ProjetoWebApplication.java
@@ -0,0 +1,13 @@
+package projeto.nelioAlves.projeto.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebApplication.class, args);
+	}
+
+}
